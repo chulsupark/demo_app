03f64424e81b82c30df3016c8618356581396b0a
Chulsu Park <chulsupark@gmail.com>
2011-09-11 20:06:52 -0400

    Initial commit with Ubuntu fix for JS in Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'b2e8166b49abfed44230c54e63610482cb360b053ce820942f1383a3fc4075e23d32e972da58f5288cc7952aa70106bb9d75d8ecf9dbe0632314cf3a40ca6c3c'
